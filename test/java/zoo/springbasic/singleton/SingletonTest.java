b6dab33b8ea16a4836893b6d3646f5d15b44aacb
1000zoo <cjswldn99@gmail.com>
2023-10-02 18:39:15 +0900

    (Section 7 - 6) 이전 테스트들 복원

--- test/java/zoo/springbasic/singleton/SingletonTest.java
+++ test/java/zoo/springbasic/singleton/SingletonTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import zoo.springbasic.AppConfig;
 import zoo.springbasic.AutoAppConfig;
+import zoo.springbasic.member.MemberService;
 import zoo.springbasic.member.MemberServiceImpl;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,12 +16,12 @@ public class SingletonTest {
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
     void pureContainer() {
-//        AppConfig appConfig = new AppConfig();
-//
-//        MemberService memberService1 = appConfig.memberService();
-//        MemberService memberService2 = appConfig.memberService();
-//
-//        assertThat(memberService1).isNotSameAs(memberService2);
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        assertThat(memberService1).isNotSameAs(memberService2);
     }
 
 
@@ -35,10 +37,10 @@ public class SingletonTest {
     @Test
     @DisplayName("싱글톤 컨테이너 테스트")
     void singletonContainerTest() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        MemberServiceImpl memberService1 = ac.getBean(MemberServiceImpl.class);
-        MemberServiceImpl memberService2 = ac.getBean(MemberServiceImpl.class);
+        MemberServiceImpl memberService1 = ac.getBean("memberService", MemberServiceImpl.class);
+        MemberServiceImpl memberService2 = ac.getBean("memberService", MemberServiceImpl.class);
 
         assertThat(memberService1).isSameAs(memberService2);
     }
